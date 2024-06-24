801be5bf7a9d932b695e6e4bc9577257a814a843
kornellen <kornel.konopka@onet.pl>
2024-06-24 17:18:52 +0200

    Dev #4 | Upgraded Fetching Products & Added Updating Products

--- backend/server/controllers/productControllers.js
+++ backend/server/controllers/productControllers.js
@@ -37,7 +37,7 @@ class Product {
       );
     });
   }
-  getProduct(req, res) {
+  getAllProduct(req, res) {
     const $getProducts = "SELECT * FROM Products";
 
     this.getConn((connect) => {
@@ -80,6 +80,74 @@ class Product {
 
           res.status(200).json({ products: products });
         });
+      });
+    });
+  }
+
+  updateProduct(req, res) {
+    const { productID } = req.params;
+    const { price, description, stockQuantity, name, category } = req.body;
+
+    const updatedAt = generalUtils.getFullCurrentDate();
+
+    if (!productID) {
+      return res.status(400).json({ error: "ProductID is required" });
+    }
+
+    const update = [];
+    if (price != "" && price != null)
+      update.push({ column: "price", value: price });
+    if (description != "" && description != null)
+      update.push({ column: "description", value: description });
+    if (stockQuantity != 0 && stockQuantity != null)
+      update.push({ column: "stockQuantity", value: stockQuantity });
+    if (name != "" && name != null)
+      update.push({ column: "name", value: name });
+    if (category != "" && category != null)
+      update.push({ column: "categoryID", value: category });
+
+    if (update.length == 0) {
+      return res.status(400).json({ error: "No fields to update" });
+    }
+
+    const updateColumn = update
+      .map((update) => `${update.column} = ?`)
+      .join(",");
+    const values = update.map((update) => update.value);
+
+    const $updateProductSQL = `UPDATE Products SET ${updateColumn}, updatedAt = ? WHERE productID = ? `;
+
+    this.getConn((connect) => {
+      connect.query(
+        $updateProductSQL,
+        [...values, updatedAt, productID],
+        (err, result) => {
+          connect.release();
+          if (err) {
+            return res.status(500).json({ error: err });
+          }
+          res.status(200).json({ info: "Product Updated Successfully" });
+        }
+      );
+    });
+  }
+
+  getProductByIDParam(req, res) {
+    const { productID } = req.params;
+
+    const $getProductByIdSQL = "SELECT * FROM products where productID = ?";
+
+    this.getConn((connect) => {
+      connect.query($getProductByIdSQL, [productID], (err, result) => {
+        connect.release();
+        if (err) {
+          res.sendStatus(500);
+          log(err);
+        }
+
+        if (result.length === 0) {
+          res.status(404).json({ error: "Product Not Found" });
+        } else res.status(200).json({ productDatas: result[0] });
       });
     });
   }
--- backend/server/routes/productRoutes.js
+++ backend/server/routes/productRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { body } = require("express-validator");
+const { body, param } = require("express-validator");
 const validateRequest = require("../middleware/validator");
 
 const ProductControllers = require("../controllers/productControllers");
@@ -20,8 +20,41 @@ router.post(
   (req, res) => productController.addProduct(req, res)
 );
 
-router.get("/getproducts", (req, res) =>
+router.get("/getallproducts", (req, res) =>
   productController.getProduct(req, res)
+);
+
+router.get(
+  "/product/:productID",
+  param("productID").isInt().withMessage("Param ProductID must be Integer"),
+  validateRequest,
+  (req, res) => productController.getProductByIDParam(req, res)
+);
+
+router.patch(
+  "/product/:productID",
+  [
+    param("productID").isInt().withMessage("Param ProductID must be Integer"),
+    body("price")
+      .optional()
+      .isDecimal()
+      .withMessage("Price must be decimal number"),
+    body("description")
+      .optional()
+      .isString()
+      .withMessage("Description must be text"),
+    body("stockQuantity")
+      .optional()
+      .isInt()
+      .withMessage("Stock Qunatity must be Integer"),
+    body("name").optional().isString().withMessage("Name must be text"),
+    body("category")
+      .optional()
+      .isInt()
+      .withMessage("Category ID is required not Category Name"),
+  ],
+  validateRequest,
+  (req, res) => productController.updateProduct(req, res)
 );
 
 module.exports = router;
